72e07993048db3fa475d55b8126c89c974638389
Pratyush <singhpratyush981@gmail.com>
2026-01-03 13:06:16 +0530

    Restrict CORS to frontend URL

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import cors from 'cors';
+const URL=process.env.FRONTEND_URL;
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:5173",
-        allowedHeaders: ["my-custom-header"],
+        origin: URL,
         credentials: true
     }
 });
